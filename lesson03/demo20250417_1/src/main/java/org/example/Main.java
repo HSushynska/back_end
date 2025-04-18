552eb88136ab1b5f492f91c9762f547f992354bc
HSushynska <stoyanova.gv@gmail.com>
2025-04-18 22:10:13 +0200

    add lesson 3

---
+++ lesson03/demo20250417_1/src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException  {
+        MyThread thread = new MyThread();
+        thread.start();
+        Thread.sleep(3000);
+        thread .interrupt(); // Отправляем запрос на прерывание
+    }
+}
