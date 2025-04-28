f555a77aeda7cc1304ee98ec7bbc17cfafa5bfbf
HSushynska <stoyanova.gv@gmail.com>
2025-04-28 21:13:50 +0200

    add homework 7

---
+++ lhomework/homework_7/task4/src/main/java/org/example/Worker.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class Worker implements Runnable{
+    private TaskBoard taskBoard;
+    private String name;
+
+    public Worker(TaskBoard taskBoard, String name) {
+        this.taskBoard = taskBoard;
+        this.name = name;
+    }
+
+    @Override
+    public void run() {
+        while (true){
+
+            Runnable task = taskBoard.getTask();
+            task.run();
+        }
+    }
+}
+
